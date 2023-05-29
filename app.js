181f48d25cc6f4b80220924e4603184443ba2baf
Thomas Koshy <ttkoshy@gmail.com>
2023-05-29 19:20:53 -0400

    some operator functionality

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ if (operatorBtns === '+') {
     divide(n1, n2);
 }
 
-
 function add(n1, n2) {
     return n1 + n2
 }
@@ -33,13 +32,49 @@ function divide(n1, n2) {
         return n1/n2
     }
 }
+
+function operate(operator, num1, num2) {
+    const n1 = Number(num1);
+    const n2 = Number(num2);
+    if (operator === '÷') {
+        currentOpScreen.textContent = divide(n1, n2)
+        console.log(divide(n1, n2))
+    } else if (operator === '×') {
+        currentOpScreen.textContent = multiply(n1, n2)
+    } else if (operator === '+') {
+        currentOpScreen.textContent = add(n1, n2)
+    } else if (operator === '-') {
+        currentOpScreen.textContent = subtract(n1, n2)
+    }
+}
 //addEventListeners to buttons;
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         const value = e.target.dataset.value;
         console.log(value);
+        currentOpScreen.textContent += value;
+
+        if (value === '=') {
+            if (currentOpScreen.textContent.includes('÷')) {
+                const equation = currentOpScreen.textContent.split('÷')
+                const numberWithoutEqSign = equation[1].split('')
+                const equation1 = numberWithoutEqSign[0]
+                operate('÷', equation[0], equation1)
+                lastOpScreen.textContent = `${equation[0]} ÷ ${equation1}`
+                console.log(equation);
+            } else if (currentOpScreen.textContent.includes('+')) {
+                const equation = currentOpScreen.textContent.split('+')
+                const numberWithoutEqSign = equation[1].split('') 
+                const equation1 = numberWithoutEqSign[0]
+                operate('+', equation[0], equation1)
+                lastOpScreen.textContent = `${equation[0]} + ${equation[1]}`
+            } else if (currentOpScreen.textContent.includes('×')) {
+                const equation = currentOpScreen.textContent.split('')
+                operate('×', equation[0], equation[2])
+                lastOpScreen.textContent = `${equation[0]} × ${equation[2]}`
+            }
+        }
     })
 })
 
-currentOpScreen.textContent = value
